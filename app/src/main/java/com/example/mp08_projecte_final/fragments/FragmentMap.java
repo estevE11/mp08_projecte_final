3b3d5654201aec6980c2fd606231da724b9e618c
Roger Esteve <rogeresteve30@gmail.com>
2021-04-14 00:51:37 +0200

    google map

--- app/src/main/java/com/example/mp08_projecte_final/fragments/FragmentMap.java
+++ app/src/main/java/com/example/mp08_projecte_final/fragments/FragmentMap.java
@@ -8,10 +8,24 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 import com.example.mp08_projecte_final.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 public class FragmentMap extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return (ViewGroup) inflater.inflate(R.layout.frag_map, container, false);
+        View view = (ViewGroup) inflater.inflate(R.layout.frag_map, container, false);
+
+        SupportMapFragment smf = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapView);
+
+        /*smf.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+
+            }
+        });*/
+        
+        return view;
     }
 }
